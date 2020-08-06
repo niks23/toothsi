c94d999d3f3da3de19cbbc90be3c9a1007970766
snikhilprdxn <nikhils@prdxn.com>
2020-08-06 17:01:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component, useState } from 'react';
+import Todos from './components/Todos'
+import uuid from 'uuid';
+import AddTodos from './components/AddTodos';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/pages/About';
+import axios from 'axios';
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
